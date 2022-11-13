540fa277573dca6572e5eddd0a3b9efd8351d8e7
ggp <ggp@helpsystems.com>
2022-11-13 14:38:10 -0800

    fix(lint-staged): don't run markdownlint on changelogs

--- packages/lint-staged-config/index.js
+++ packages/lint-staged-config/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 module.exports = {
-  '*.md': ['prettier --cache --write', 'markdownlint'],
+  'CHANGELOG.md': ['prettier --cache --write'],
+  '!(CHANGELOG).md': ['prettier --cache --write', 'markdownlint'],
   '*.{js,jsx,ts,tsx}': ['prettier --cache --write', 'eslint --cache --fix'],
   '*.json': ['prettier --cache --write'],
   '*.{css,scss}': ['prettier --cache --write', 'stylelint --cache --fix'],
